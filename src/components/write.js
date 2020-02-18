8a4b75dd2848e076fb3dedcc269c08a6279f2ba7
masesisaac <masesisaac@gmail.com>
2020-02-18 16:16:29 +0300

    fix props on Write

--- src/components/write.js
+++ src/components/write.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { View, TextInput, StyleSheet, Text } from "react-native";
 
-const Write = ({ onChangeText, value, placeholder, multiline, textBefore }) => {
+const Write = ({ textBefore, ...rest}) => {
   return (
     <View style={styles.writeContainer}>
       {
         textBefore ? (<Text style={{marginRight: 5}}>{textBefore}</Text>) : null
       }
       <TextInput
-        placeholder={placeholder}
         autoFocus
-        multiline={multiline}
         maxLength={100}
         style={styles.write}
-        value={value}
-        onChangeText={text => onChangeText(text)}
+        {...rest}
       />
     </View>
   );
